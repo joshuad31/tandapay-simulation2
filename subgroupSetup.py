1285b17739491a54deeabc5492608c61d5224f9c
fornzltoth <kevinncosby2@gmail.com>
2023-07-28 19:20:22 -0400

    implemented unit test for subgroupSetup

--- subgroupSetup.py
+++ subgroupSetup.py
@@ -1,5 +1,9 @@
 import math
 
+from enum import Enum
+from userRecord import UserRecord
+
+# subgroupSetup method. Documentation can be found at: https://www.notion.so/Subgroup-Setup-7e7852c10715423699fc522ca9e6536a
 def subgroupSetup(total_member_cnt, user_list)
     assert isinstance(total_member_cnt, int), "total_member_cnt must be an integer!"
     assert isinstance(user_list, list), "user_list must be a list!"
@@ -11,7 +15,7 @@ def subgroupSetup(total_member_cnt, user_list)
     num_five_member_groups *= 5
 
     # step 3 calculations (remaining members)
-    num_remaining_members = total_member_cnt - num_five_member_groups2
+    num_remaining_members = total_member_cnt - num_five_member_groups
     
     # step 4 calculations (number of 6-member groups)
     num_six_member_groups = num_remaining_members / 6
@@ -37,13 +41,13 @@ def subgroupSetup(total_member_cnt, user_list)
     num_remaining_members = num_remaining_members % 4
 
     # step 9 adjustments
-    if num_remaining_members == 1
+    if num_remaining_members == 1:
         num_five_member_groups -= 1
         num_six_member_groups += 1
-    elif num_remaining_members == 2
+    elif num_remaining_members == 2:
         num_five_member_groups -= 1
         num_seven_member_groups += 1
-    elif num_remaining_members == 3
+    elif num_remaining_members == 3:
         num_five_member_groups -= 1
         num_four_member_groups += 2
 
@@ -71,96 +75,33 @@ def subgroupSetup(total_member_cnt, user_list)
                 i += 1  # move to the next user
             group_num += 1
 
+# unit test for subgroupSetup function
+def test_subgroupSetup():
+    # create a list of 100 userRecord objects
+    user_list = [UserRecord() for _ in range(100)]
 
-#### TODO: delete function below; function was only pasted here for reference while writing replacement function
+    # call your function with the user_list
+    subgroupSetup(len(user_list), user_list)
 
-def subgroup_setup(self):
-    # ============================== Subgroup Setup ========================================
-    step1_ev1 = self._total
-    step2 = self._total / 5
-    step3 = round(step2 / 2.3333)
-    step4 = step3 * 5
-    step5 = step1_ev1 - step4
-    step6 = step5 / 6
-    step7 = round(step6 / 2)
-    step8 = step7 * 6
-    step9 = step5 - step8
-    step10 = step9 / 7
-    step11 = int(step10 / 2)
-    step12 = step11 * 7
-    step13 = step9 - step12
-    step14 = int(step13 / 4)
-    step15 = step13 % 4
-    if step15 == 1:
-        step3 -= 1
-        step7 += 1
-    elif step15 == 2:
-        step3 -= 1
-        step11 += 1
-    elif step15 == 3:
-        step3 -= 1
-        step14 += 2
-
-    # Assigning number to the group, condition checking for group == 4
+    # check the users are correctly assigned to groups and the properties are set correctly
     group_num = 1
-    group_mem_count = 0
-    temp_val_four = step14 * 4
-    offset = 0
-    for i in range(temp_val_four):
-        self.usr[i + offset]['orig_sbg_num'] = group_num
-        self.usr[i + offset]['remaining_orig_sbg'] = 4
-        self.usr[i + offset]['cur_sbg_num'] = group_num
-        self.usr[i + offset]['members_cur_sbg'] = 4
-        group_mem_count += 1
-        if group_mem_count == 4:
+    group_size = 4
+    for i, user in enumerate(user_list):
+        if i % group_size == 0 and i > 0:
             group_num += 1
-            group_mem_count = 0
-    offset += temp_val_four
+            if group_num <= num_four_member_groups:
+                group_size = 4
+            elif group_num <= num_four_member_groups + num_five_member_groups:
+                group_size = 5
+            elif group_num <= num_four_member_groups + num_five_member_groups + num_six_member_groups:
+                group_size = 6
+            else:
+                group_size = 7
+        assert user.orig_sbg_num == group_num, f"User at index {i} is in wrong group"
+        assert user.remaining_orig_sbg == group_size, f"User at index {i} has wrong remaining group size"
+        assert user.cur_sbg_num == group_num, f"User at index {i} has wrong current group number"
+        assert user.members_cur_sbg == group_size, f"User at index {i} has wrong current group size"
 
-    # condition checking for group == 5
-    temp_val_five = step3 * 5
-    for i in range(temp_val_five):
-        self.usr[i + offset]['orig_sbg_num'] = group_num
-        self.usr[i + offset]['remaining_orig_sbg'] = 5
-        self.usr[i + offset]['cur_sbg_num'] = group_num
-        self.usr[i + offset]['members_cur_sbg'] = 5
-        group_mem_count += 1
-        if group_mem_count == 5:
-            group_num += 1
-            group_mem_count = 0
-    offset += temp_val_five
+    print("All tests passed!")
 
-    # condition checking for group == 6
-    temp_val_six = step7 * 6
-    for i in range(temp_val_six):
-        self.usr[i + offset]['orig_sbg_num'] = group_num
-        self.usr[i + offset]['remaining_orig_sbg'] = 6
-        self.usr[i + offset]['cur_sbg_num'] = group_num
-        self.usr[i + offset]['members_cur_sbg'] = 6
-        group_mem_count += 1
-        if group_mem_count == 6:
-            group_num += 1
-            group_mem_count = 0
-    offset += temp_val_six
 
-    # condition checking for group == 7
-    temp_val_seven = step11 * 7
-    for i in range(temp_val_seven):
-        self.usr[i + offset]['orig_sbg_num'] = group_num
-        self.usr[i + offset]['remaining_orig_sbg'] = 7
-        self.usr[i + offset]['cur_sbg_num'] = group_num
-        self.usr[i + offset]['members_cur_sbg'] = 7
-        group_mem_count += 1
-        if group_mem_count == 7:
-            group_num += 1
-            group_mem_count = 0
-
-    checksum = offset + temp_val_seven
-    if checksum != self._total:
-        raise ValueError(f"Initial group checksum failed: checksum:{checksum} != EV1:{self._total}")
-    logger.debug({"D": temp_val_four, "A": temp_val_five, "B": temp_val_six, "C": temp_val_seven})
-
-    logger.debug(
-        f'Group4 members: {step14}, Group5 members: {step3}, Group6 members: {step7}, '
-        f'Group7 members: {step11}, Total group: {step14 * 4 + step3 * 5 + step7 * 6 + step11 * 7}')
-    return temp_val_four
--- userRecord.py
+++ userRecord.py
@@ -1,3 +1,4 @@
+from enum import Enum
 
 class ValidityEnum(Enum):
     VALID = 0
