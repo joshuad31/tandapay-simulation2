08acba191257d09ea79d33e5b82afed07a921a07
fornzltoth <kevinncosby2@gmail.com>
2023-08-02 19:30:57 -0400

    removed magic numbers in uf2

--- uf2_pricing_function.py
+++ uf2_pricing_function.py
@@ -1,4 +1,6 @@
-
+from environmentVariables import EnvironmentVariables
+from pricingVariables import PricingVariables
+from systemRecord import SystemRecord
 from utility import remove_user
 
 def uf2_pricing_function(env_vars, sys_rec, pricing_vars, user_list, current_period):
@@ -61,7 +63,7 @@ def uf2_pricing_function(env_vars, sys_rec, pricing_vars, user_list, current_per
                     leave_list.append(user_index)
             
             # 1e. ELSE evaluate user for cumulative_increase_percentage. ONLY run in periods 5 - 10
-            else if cumulative_increase_percentage(env_vars, pricing_vars, user, current_period):
+            elif cumulative_increase_percentage(env_vars, pricing_vars, user, current_period):
                 leave_list.append(user_index)
         # 2. ELSE IF total_value_refund (current period) is NOT equal to 0
         elif user.total_value_refund_list[current_period] != 0:
@@ -86,78 +88,27 @@ def uf2_pricing_function(env_vars, sys_rec, pricing_vars, user_list, current_per
         # 4. since sys_rec.skipped_cnt uses a setter, it will calculate skip_shortfall automatically
         # when skipped_cnt is updated. Therefore, no code is needed here.
 
-def cumulative_increase_percentage(env_vars, pricing_vars, user, current_period)
+def cumulative_increase_percentage(env_vars, pricing_vars, user, current_period, LOWER_PERIOD = 4, UPPER_PERIOD = 9, NUM_MONTHS_TO_AVG = 3):
     # run only if the period is 5-10
-    if not (4 <= current_period <= 9)
+    if not (LOWER_PERIOD <= current_period <= UPPER_PERIOD):
         return False
     
     # 1. calculate average for current_month_sec_calc for last 3 periods
     debug_total_iterations = 0
     average = 0
-    for i in range(current_period, current_period - 3, -1)
+    for i in range(current_period, current_period - NUM_MONTHS_TO_AVG, -1):
         average += user.current_month_second_calc_list[i]
 
-    assert debug_total_iterations == 3, "loop iterated more than 3 times! spec calls for last 3 months average!"
-    average /= 3
+    assert debug_total_iterations == NUM_MONTHS_TO_AVG, f"loop iterated more than {NUM_MONTHS_TO_AVG} times!"
+    average /= NUM_MONTHS_TO_AVG
 
     # 2. cumulative_increase_percentage = (average / (cov_req / total_member_cnt)) - 1
     cumulative_increase_percentage = (average / (env_vars.cov_req / env_vars.total_member_cnt)) - 1
     
     if cumulative_increase_percentage > pricing_vars.prem_inc_cum:
         random_number = random.uniform(0, 1)
-        if random_number < pricing_vars.ph_leave_cum
+        if random_number < pricing_vars.ph_leave_cum:
             return True
         else:
             user.cur_status = CurrentStatusEnum.PAID
             return False
-     
-
-#def user_func_2(self):
-#        """"
-#        Pay Stage 2,  Pricing function
-#        """
-#        slope = (self.pv['ph_leave_ceiling'] - self.pv['ph_leave_floor']) / \
-#                (self.pv['prem_inc_ceiling'] - self.pv['prem_inc_floor'])
-#        leave_users = []
-#        for i in self._active_users():
-#            if self.usr[i]['total_value_refunds'][self.period] == 0:
-#                matches = [p for p in range(self.period - 1, -1, -1) if self.usr[i]['total_value_refunds'][p] == 0]
-#            else:
-#                matches = [p for p in range(self.period - 1, -1, -1) if self.usr[i]['total_value_refunds'][p] != 0]
-#            if not matches:
-#                logger.debug(f"User {i} - cannot find the matching period!")
-#                continue
-#            # print(f"The user number is {i}")
-#            # print(f"The current month sec calc for this period {self.usr[i]['cur_mon_sec_cals'][self.period]}")
-#            # print(f"The current month sec calc for matching period {self.usr[i]['cur_mon_sec_cals'][matches[-1]]}")
-#            if self.usr[i]['cur_mon_sec_cals'][self.period - 1] == 0:
-#                logger.warning(f"Pricing Function: User{i} sec cals in period {self.period} "
-#                               f"- {self.usr[i]['cur_mon_sec_cals']}")
-#                continue
-#            one_mon_inc_perc = \
-#                (self.usr[i]['cur_mon_sec_cals'][self.period] / self.usr[i]['cur_mon_sec_cals'][self.period - 1]) - 1
-#            # print(f"The one_month_inc_perc is {one_mon_inc_perc}")
-#            one_mon_inc_perc = min(one_mon_inc_perc, self.pv['prem_inc_ceiling'])
-#            if one_mon_inc_perc >= self.pv['prem_inc_floor']:
-#                ph_skip_perc = slope * (one_mon_inc_perc - self.pv['prem_inc_floor']) + self.pv['ph_leave_floor']
-#                if not self.pv['ph_leave_floor'] <= ph_skip_perc <= self.pv['ph_leave_ceiling']:
-#                    raise ValueError(f"Something is wrong with PH skip percentage - {ph_skip_perc}")
-#                rando = random.uniform(0, 1)
-#                if rando < ph_skip_perc:
-#                    # logger.debug(f'Run {self.period} PH Skip Pct: {ph_skip_perc} User: {i}, {one_mon_inc_perc}, '
-#                    #              f'Random num: {rando}')
-#                    leave_users.append(i)
-#                    continue
-#            cum_inc_perc = self.usr[i]['cur_mon_sec_cals'][self.period] / (self.cov_req / self._total) - 1
-#            if cum_inc_perc > self.pv['prem_inc_cum']:
-#                if random.uniform(0, 1) < self.pv['ph_leave_cum']:
-#                    leave_users.append(i)
-#
-#        for i in leave_users:
-#            self.sys[self.period]['valid_remaining'] -= 1
-#            self.sys[self.period]['skipped_cnt'] += 1
-#            self.remove_usr(i, reason='pricing')
-#        sd = self.sys[self.period]
-#        self.sys[self.period]['skip_sf'] = sd['skipped_cnt'] * sd['cur_mon_1st_calc']
-
-
